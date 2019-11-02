81e12df7409c0328121afd3b62b6d026f51839bb
L.Rain <bxy2997@sina.com>
2019-11-02 17:35:29 +0800

    finish

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "react-notifications/lib/notifications.css";
+import "./App.css";
+import { NotificationContainer } from "react-notifications";
+import Login from "./Login";
+import Chat from "./Chat";
+
+const App = () => {
+  const [user, setUser] = useState(null);
+  const renderApp = () => {
+    if (user) {
+      return <Chat user={user} />;
+    } else {
+      return <Login setUser={setUser} />;
+    }
+  };
+  return <div className='container'>{renderApp()}</div>;
+};
+export default App;
---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = {
+    appID: '10434fe7ff68460',
+    apiKey: '89119dc7cc2e2b83695737a38666ae012f6d337d',
+  };
+  
+  export default config;
